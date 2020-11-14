a0a60fb220299502fda49f9667835af64b651184
Adithyajp <adiyjp@gmail.com>
2020-11-14 12:37:59 +0530

    intial commmit navbar complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './App.css';
+
+//components
+import MyNavbar from "./components/my-navbar/navbar.component";
+
+const App = () => {
+  return (
+    (<div >
+      <MyNavbar />
+      My Portfolio
+    </div>)
+  );
+}
+
+export default App;
